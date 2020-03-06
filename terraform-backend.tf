d7aa2ae19c7fb59e3d3af187a70fa0ee6e6c2b78
Saurabh <bksaurabh96@gmail.com>
2020-03-06 18:31:18 +0530

    Update terraform-backend.tf

--- terraform-backend.tf
+++ terraform-backend.tf
@@ -1,9 +1,4 @@
 terraform {
   required_version = ">= 0.12"
-  backend "azurerm" {
-    resource_group_name  = "azure-sandbox"
-    storage_account_name = "sandboxdevtfstates"
-    container_name       = "tfstates"
-    key                  = "terraform-getting-started.tfstate"
-  }
+  backend "azurerm" {}
 }
