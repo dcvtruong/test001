3533de29836a5e63223cc29133d60dbbe406fff6
dcvtruong <45860078+dcvtruong@users.noreply.github.com>
2020-03-11 14:21:33 -0500

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -2,7 +2,9 @@ provider "azurerm" {
   version         = "1.36.0"
 }
 
-variable "rg_name" {}
+variable "rg_name" {
+  rg_name = "ity-cc-cloud-sandbox"  
+}
 
 resource "azurerm_resource_group" "example" {
   name     = "${var.rg_name}-rg"
