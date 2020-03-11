5923f420fd79db200e1ca62f4821e9eabdcc0e2a
Saurabh <bksaurabh96@gmail.com>
2020-03-11 21:34:43 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   version         = "1.36.0"
+  tenant_id       = var.TENANT_ID
+  subscription_id = var.SUBSCRIPTION_ID
+  client_id       = var.CLIENT_ID
+  client_secret   = var.CLIENT_SECRET
 }
 
 resource "azurerm_resource_group" "example" {
