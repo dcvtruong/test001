dc5623132775c2dcfea9d8d6724f8d5100eff4c6
Saurabh <bksaurabh96@gmail.com>
2020-03-12 00:30:41 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -2,6 +2,8 @@ provider "azurerm" {
   version         = "1.36.0"
 }
 
+variable "rg_name" {}
+
 resource "azurerm_resource_group" "example" {
   name     = "${var.rg_name}-rg"
   location = "West US"
